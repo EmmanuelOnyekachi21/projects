97218abfbcbf60258c4a854a2b8c4a6a422845ba
EmmanuelOnyekachi21 <emmanuelonyekachi04122000@gmail.com>
2024-04-02 03:21:44 -0400

    created a method, `_update_bullets()` and moved both the for statement and self.bullet.update into it.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -30,16 +30,19 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # print(len(self.bullets))
+            
 
-            self.bullets.update()
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update()
 
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
